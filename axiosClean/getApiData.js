a622260eccf5a38cad1c7bd35b2da39c392c8b20
Todd Garrison <toddlgarrison@gmail.com>
2023-11-10 16:54:26 -0500

    create getApiData.js

---
+++ axiosClean/getApiData.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+import apiKey from '../config/config.js'
+
+async function getApiData(url) {
+    const apiUrl = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=10`;
+
+    try {
+        const response = await axios.get(apiUrl)
+        console.log(response)
+    } catch (error) {
+        console.error(`Error getting API data: ${error}`)
+    }
+}
+
+getApiData()
+
+export default getApiData
