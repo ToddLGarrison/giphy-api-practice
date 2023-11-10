3c97d5eb2af87a6ccfa4b58b0d465cc60f096ecb
Todd Garrison <toddlgarrison@gmail.com>
2023-11-10 12:24:27 -0500

    get gifs by frames

---
+++ axiosSearch/getGiphyBasedOnSpeed.js
@@ -0,0 +1,37 @@
+import getGiphyData from "./getGiphyData.js";
+
+async function getGiphyBasedOnSpeed() {
+    const gifs = await getGiphyData();
+
+    const lowSpeedThreshold = 60
+    const mediumSpeedThreshold = 125
+    const highSpeedThreshold = 200
+
+    
+    //get giphy original frames data
+    // const giphyFrames = []
+    // for (const gif of gifs) {
+        //     // console.log('Gifs images details:', gif.images.original.frames)
+        // }
+        
+    const lowSpeedCount = gifs.filter((gif) => gif.images.original.frames <= 50).length
+    const mediumSpeedCount = gifs.filter((gif) => gif.images.original.frames <= 125).length
+    const highSpeedCount = gifs.filter((gif) => gif.images.original.frames <= 200).length
+
+    console.log(
+        'Low Speed Count', lowSpeedCount,
+        'Medium Speed Count', mediumSpeedCount,
+        'High Speed Count', highSpeedCount,   
+    )
+
+
+    return {
+        'Low Speed Count': lowSpeedCount,
+        'Medium Speed Count': mediumSpeedCount,
+        'High Speed Count': highSpeedCount,   
+    }
+}
+
+getGiphyBasedOnSpeed()
+
+export default getGiphyBasedOnSpeed
